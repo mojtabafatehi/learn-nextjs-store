ab56f7b924a9e8da4af11fe009350d77d8421e4a
mojtabafatehi <m.fatehi.1383@gmail.com>
2025-06-19 18:05:38 +0330

    Add ProductCard component

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,9 +1,59 @@
 import Container from "@/components/Container";
+import ProductItem from "@/components/ProductItem";
 
 export default function Store() {
+  // Sample data for products
+  const date = [
+    {
+      id: "1",
+      image:
+        "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTExL3BmLXMxMDgtcG0tNDExMy1tb2NrdXAuanBn.jpg",
+      title: "محصول 1",
+      description: "توضیحات محصول 1",
+      price: 100000,
+    },
+    {
+      id: "2",
+      image:
+        "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTExL3BmLXMxMDgtcG0tNDExMy1tb2NrdXAuanBn.jpg",
+      title: "محصول 2",
+      description: "توضیحات محصول 2",
+      price: 200000,
+    },
+    {
+      id: "3",
+      image:
+        "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTExL3BmLXMxMDgtcG0tNDExMy1tb2NrdXAuanBn.jpg",
+      title: "محصول 3",
+      description: "توضیحات محصول 3",
+      price: 300000,
+    },
+    {
+      id: "4",
+      image:
+        "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTExL3BmLXMxMDgtcG0tNDExMy1tb2NrdXAuanBn.jpg",
+      title: "محصول 4",
+      description: "توضیحات محصول 4",
+      price: 400000,
+    },
+    {
+      id: "5",
+      image:
+        "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTExL3BmLXMxMDgtcG0tNDExMy1tb2NrdXAuanBn.jpg",
+      title: "محصول 5",
+      description: "توضیحات محصول 5",
+      price: 500000,
+    },
+  ];
+
   return (
     <Container>
       <h1>فروشگاه</h1>
+      <div className="grid grid-cols-4 gap-4">
+        {date.map((item) => (
+          <ProductItem key={item.id} {...item} />
+        ))}
+      </div>
     </Container>
   );
 }
---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,25 @@
+interface IProductItemProps {
+  id: string;
+  image: string;
+  title: string;
+  description: string;
+  price: number;
+}
+
+export default function ProductItem({
+  title,
+  image,
+  price,
+}: IProductItemProps) {
+  return (
+    <div className="shadow-md">
+      <img src={image} className="w-full h-64 object-cover mb-4" />
+      <div className="p-4">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <span className="text-lg font-semibold text-green-600">
+          قیمت: {price} تومان
+        </span>
+      </div>
+    </div>
+  );
+}
