871932a3a08a82df30f00bdf6025a272040d2663
mojtabafatehi <m.fatehi.1383@gmail.com>
2025-06-27 19:27:42 +0330

    Implement discount code logic and final price calculation in cart

--- src/components/ListCartItem.tsx
+++ src/components/ListCartItem.tsx
@@ -7,9 +7,19 @@ import { IProductItemProps } from "./ProductItem";
 import { formatNumberWithCammas } from "@/utils/number";
 import Link from "next/link";
 
+interface IDiscount {
+  code: string;
+  id: number;
+  percentage: number;
+}
+
 export default function ListCartItem() {
   const { CartItems } = useShoppingCartContext();
   const [data, setData] = useState<IProductItemProps[]>();
+  const [discountCode, setDiscountCode] = useState("");
+  const [discountPrice, setDiscountPrice] = useState(0);
+  const [finalPrice, setFinalPrice] = useState(0);
+
   useEffect(() => {
     axios(`http://localhost:8001/products`)
       .then((result) => {
@@ -20,6 +30,27 @@ export default function ListCartItem() {
         console.error("خطا در گرفتن محصول:", err.message);
       });
   }, []);
+
+  let totalPrice = CartItems.reduce((total, item) => {
+    let price = data?.find((product) => product.id == item.id)?.price || 0;
+
+    return total + price * item.qty;
+  }, 0);
+
+  function handleDiscount() {
+    axios(`http://localhost:8001/discounts?code=${discountCode}`).then(
+      (result) => {
+        const data = result.data as IDiscount[];
+
+        console.log(totalPrice);
+        let discountPR = (totalPrice * (data[0]?.percentage || 0)) / 100;
+        let finalPrice = totalPrice - discountPR;
+
+        setDiscountPrice(discountPR);
+        setFinalPrice(finalPrice);
+      }
+    );
+  }
 
   if (!CartItems.length) {
     return (
@@ -47,30 +78,30 @@ export default function ListCartItem() {
         <div>
           <h3>
             قیمت کل:
-            <span>
-              {formatNumberWithCammas(
-                CartItems.reduce((total, item) => {
-                  let price =
-                    data?.find((product) => product.id == item.id)?.price || 0;
-
-                  return total + price * item.qty;
-                }, 0)
-              )}
-            </span>
+            <span>{formatNumberWithCammas(totalPrice)}</span>
           </h3>
           <h3>
-            سود شما از این خرید: <span>22</span>
+            سود شما از این خرید:{" "}
+            <span>{formatNumberWithCammas(discountPrice)}</span>
           </h3>
           <h3>
-            قیمت نهایی: <span>77</span>
+            قیمت نهایی: <span>{formatNumberWithCammas(finalPrice)}</span>
           </h3>
         </div>
         <div className="mt-4">
           <input
             className=" border-amber-300 border-2 p-2 mx-2 rounded"
             placeholder="کد تخفیف را وارد کنید"
+            onChange={(e) => {
+              setDiscountCode(e.target.value);
+            }}
           ></input>
-          <button className="px-3 py-1 bg-red-400 text-white rounded cursor-pointer">
+          <button
+            className="px-3 py-1 bg-blue-400 text-white rounded cursor-pointer"
+            onClick={() => {
+              handleDiscount();
+            }}
+          >
             اعمال کد تخفیف
           </button>
         </div>
